963c3dd3262fc9b613cd34fb2b7c9841ac471827
juan carlos alvez <jcalvez@hotmail.com>
2024-10-10 12:14:32 -0300

    Second commit

---
+++ src/Restricted.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Restricted = () => {
+  return (
+    <>
+      <p>Website Restricted By Developer</p>
+      <p>Due To Incomplete Payment Of</p>
+      <p>Development.</p>
+    </>
+  );
+};
+
+export default Restricted;
